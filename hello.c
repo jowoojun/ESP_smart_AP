79b5107e1481936de3368937c0ade8c22924cb9b
ChangGunLee <cd8114@gmail.com>
2017-07-11 22:19:36 +0900

    setting ssid and pwd Ccode and shell script

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
- 	printf("오늘 따라 랙이 너무 심해\n");
-	printf("hello\n"); 
-return 0;
-}
--- sleep.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main (){
-	int i = 0;
-	for(i = 0 ; i < 3 ; i++){
-		printf("I want to sleep \n");
-	} 
-	sleep(10);
-	return 0;
-}
