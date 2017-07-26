2f7c8f9e670f2996dd7cefc6681af21634e09418
kang da sol <dskang07085@gmail.com>
2017-07-26 18:16:19 +0900

    network config

--- front-end/js/advanced.js
+++ front-end/js/advanced.js
@@ -8,6 +8,7 @@ $(function(){
     });
 });
 
+
 $(function(){
   $("ul.tab li ul li").click(function(){
     var active_tab=$(this).attr("data-tab");
@@ -17,4 +18,42 @@ $(function(){
     $('#'+active_tab).addClass('current');
     $('#'+active_tab+'_desc').addClass('current');
   })
+});
+
+
+$(document).ready(function(){
+  $("input[name='con_type']:radio").change(function(){
+    //get radio value
+    var con_type=this.value;
+    if(con_type=="ip_dynamic"){
+      $("#ip_dynamic_cont").show();
+      $("#ip_static_cont").hide();
+    }else if(con_type=="ip_static"){
+      $("#ip_static_cont").show();
+      $("#ip_dynamic_cont").hide();
+    }
+  });
+
+  $("input[name='dns_server']:radio").change(function(){
+      var dns_server=this.value;
+      if(dns_server=="dns_dynamic"){
+        //readonly
+        $(".dns_default").attr("readonly",true);
+        $(".dns_secondary").attr("readonly",true);
+      }else if(dns_server=="dns_static"){
+        $(".dns_default").attr("readonly",false);
+        $(".dns_secondary").attr("readonly",false);
+      }
+  });
+
+  $("input[name='dhcp']:radio").change(function(){
+      var dhcp=this.value;
+      if(dhcp=="stop"){
+        //readonly
+        $(".dhcp_range").attr("readonly",true);
+      }else if(dns_server=="dns_static"){
+        $(".dhcp_range").attr("readonly",false);
+      }
+  });
+
 });
