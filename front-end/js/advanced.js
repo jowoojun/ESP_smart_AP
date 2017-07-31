b3b1146a1475b8ea00d37aeda103e0982a2c46e2
kang da sol <dskang07085@gmail.com>
2017-07-31 10:57:31 +0900

    add some code

--- front-end/js/advanced.js
+++ front-end/js/advanced.js
@@ -20,6 +20,52 @@ $(function(){
   })
 });
 
+
+$(function(){
+  $("#add_word").click(function(){
+    if($('#word_list').find("[value='"+$('#searchbox').val().replace('"','\'')+"']").length == 0){
+      $('#word_list').append('<option value="'+$('#searchbox').val().replace('"','\'')+'">'+$('#searchbox').val()+"</option>");
+    }else{
+      alert("등록된 단어입니다.");
+    }
+  });
+
+  $("#del_word").click(function(){
+    $('#word_list option:selected').each( function() {
+        $(this).remove();
+    });
+  });
+});
+
+$(function(){
+  $("#mac_add").click(function(){
+    //mac주소 만들기
+    function full_mac(){
+      var full_mac="";
+      for(i=1;i<6;i++){
+        full_mac+=$("#black_mac"+i).val()+"-";
+      }
+      full_mac+=$("#black_mac6").val();
+      return full_mac;
+    }
+    var full_mac=full_mac();
+
+    //추가하기
+    if($('#mac_blacklist').find("[value='"+full_mac+"']'").length == 0){
+      $('#mac_blacklist').append('<option value="'+full_mac+'">'+full_mac+"</option>");
+    }else{
+      alert("등록된 MAC주소 입니다.");
+    }
+  });
+
+  //삭제하기
+  $("#mac_del").click(function(){
+    $('#mac_blacklist option:selected').each( function() {
+        $(this).remove();
+    });
+  });
+});
+
 $(document).ready(function(){
   $("input[name='con_type']:radio").change(function(){
     //get radio value
@@ -56,19 +102,56 @@ $(document).ready(function(){
   });
 
   $("#chk_all").click(function(){
-      $("input[name=mal_site_category]").prop("checked",true);
+      $("input[name='mal_site_category']").attr("checked",true);
   });
 
   $("#chk_not_all").click(function(){
-    $("input[name=mal_site_category]").prop("checked",false);
+    $("input[name='mal_site_category']").attr("checked",false);
   });
 
   $("#chk_all_day").click(function(){
-      $("input[name=day]").prop("checked",true);
+      $("input[name='day']").attr("checked",true);
   });
 
   $("#chk_not_all_day").click(function(){
-    $("input[name=day]").prop("checked",false);
+    $("input[name='day']").attr("checked",false);
   });
+
+  $("#word_all").click(function(){
+      $("#word_list > option").attr("selected",true);
+  });
+
+  $("#word_not_all").click(function(){
+    $("#word_list > option").attr("selected",false);
+  });
+
+  $("#blacklist_all").click(function(){
+      $("#mac_blacklist > option").attr("selected",true);
+  });
+
+  $("#blacklist_not_all").click(function(){
+    $("#mac_blacklist > option").attr("selected",false);
+  });
+
+  var availableTags=[
+    '강다솔',
+    '조우준',
+    '김재한',
+    '장호동',
+    '이창건',
+    '안랩',
+    '한이음',
+    '박수현멘토님',
+    '강민아',
+    '조석현',
+    '김수빈'
+  ];
+
+  $("#searchbox").autocomplete(availableTags,{
+    matchContains: true,
+    selectFirst: false
+  });
+
+  
 
 });
