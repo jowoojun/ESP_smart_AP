244b7b1cfad2dd2b8f71dba2c3ee86dc0aa10428
kang da sol <dskang070@naver.com>
2017-08-27 23:57:54 +0900

    edit general setting

--- front-end/js/advanced.js
+++ front-end/js/advanced.js
@@ -37,35 +37,6 @@ $(function(){
   });
 });
 
-$(function(){
-  $("#mac_add").click(function(){
-    //mac주소 만들기
-    function full_mac(){
-      var full_mac="";
-      for(i=1;i<6;i++){
-        full_mac+=$("#black_mac"+i).val()+"-";
-      }
-      full_mac+=$("#black_mac6").val();
-      return full_mac;
-    }
-    var full_mac=full_mac();
-
-    //추가하기
-    if($('#mac_blacklist').find("[value='"+full_mac+"']'").length == 0){
-      $('#mac_blacklist').append('<option value="'+full_mac+'">'+full_mac+"</option>");
-    }else{
-      alert("등록된 MAC주소 입니다.");
-    }
-  });
-
-  //삭제하기
-  $("#mac_del").click(function(){
-    $('#mac_blacklist option:selected').each( function() {
-        $(this).remove();
-    });
-  });
-});
-
 $(document).ready(function(){
   $("input[name='con_type']:radio").change(function(){
     //get radio value
@@ -117,6 +88,46 @@ $(document).ready(function(){
     $("input[name='day']").attr("checked",false);
   });
 
+  $(function(){
+    $("#mac_add").click(function(){
+      //mac주소 만들기
+      function full_mac(){
+        var full_mac="";
+        for(i=1;i<6;i++){
+          full_mac+=$("#black_mac"+i).val()+"-";
+        }
+        full_mac+=$("#black_mac6").val();
+        return full_mac;
+      }
+      var full_mac=full_mac();
+
+      //추가하기
+      if($('#mac_blacklist').find("[value='"+full_mac+"']'").length == 0){
+        $('#mac_blacklist').append('<option value="'+full_mac+'">'+full_mac+"</option>");
+      }else{
+        alert("등록된 MAC주소 입니다.");
+      }
+    });
+
+    //삭제하기
+    $("#mac_del").click(function(){
+      $('#mac_blacklist option:selected').each( function() {
+          $(this).remove();
+      });
+    });
+  });
+
+  $(document).ready(function(){
+
+  	$("#blacklist_all").click(function(){
+  	      $("#mac_blacklist > option").attr("selected",true);
+  	  });
+
+  	  $("#blacklist_not_all").click(function(){
+  	    $("#mac_blacklist > option").attr("selected",false);
+  	  });
+  });
+
   $("#word_all").click(function(){
       $("#word_list > option").attr("selected",true);
   });
@@ -152,6 +163,6 @@ $(document).ready(function(){
     selectFirst: false
   });
 
-  
+
 
 });
