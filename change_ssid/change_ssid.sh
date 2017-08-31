6fdfbd68551b8a7afc25e645044e445f368a1c2b
ChanggunLEE <cd8114@gmail.com>
2017-09-01 01:08:02 +0900

    change some files

--- change_ssid/change_ssid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# run change_ssid.c
-sudo ./change_ssid $1
-
-# remove conf file and change bak file to conf file
-sudo rm /etc/hostapd/hostapd.conf
-sudo mv /etc/hostapd/hostapd.bak /etc/hostapd/hostapd.conf
-
