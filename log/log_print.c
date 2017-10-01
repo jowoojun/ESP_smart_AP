c24735a1176b4c7035078df1b4c510cfd49334b2
ChanggunLee <cd8114@gmail.com>
2017-10-01 22:46:57 +0900

    Add printf log code

---
+++ log/log_print.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#define MAX 100
+
+void print_log(char * log){
+    FILE * read;
+    char str[MAX];
+    time_t time;
+
+    read = fopen("log.txt","a");
+    if(!read){
+        printf("can't open the file\n");
+    }
+
+    strcpy(str,log);
+    strcat(str," : ");
+    strcat(str,ctime(&time));
+    
+    fprintf( read , "%s\n" , str );
+    
+    fclose(read);
+   
+    return ;
+}
+
+int main( int argc , char * argv[] ){
+    
+    print_log(argv[1]);
+
+    return 0;
+}
