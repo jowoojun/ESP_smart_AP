2ec6819fb340cdff5a934087b0f19f78a4824705
ChanggunLEE <cd8114@gmail.com>
2017-10-02 22:33:02 +0900

    Add install remove shellscript

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# admin setting
+gcc -o admin/change_ssid admin/change_ssid.c
+gcc -o admin/change_pwd admin/change_pwd.c
+
+# ip setting
+gcc -o ip/change_ip ip/change_ip.c
+gcc -o ip/change_netmask ip/change_netmask.c
+gcc -o ip/change_DHCPrange ip/change_DHCPrange.c
+
+# time setting
+gcc -o time_set/time_set time_set/time_set.c
+
---
+++ remove.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm admin/change_ssid
+rm admin/change_pwd
+
+rm ip/change_ip
+rm ip/change_netmask
+rm ip/change_DHCPrange
+
+rm time_set/time_set
