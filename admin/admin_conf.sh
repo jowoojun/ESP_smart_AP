bd2446bef8d76750fcf43f8649c8d54b038d6c9c
ChanggunLEE <cd8114@gmail.com>
2017-10-06 23:12:00 +0900

    Add bakup code in amdin file

--- admin/admin_conf.sh
+++ admin/admin_conf.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 # run change_ssid.c
-sudo ./change_ssid $1
+sudo /home/pi/project/esp_smart_ap/admin/change_ssid $1
 
 # remove conf file and change bak file to conf file
 sudo rm /etc/hostapd/hostapd.conf
 sudo mv /etc/hostapd/hostapd.bak /etc/hostapd/hostapd.conf
-
-
-# Add a log text in log file
-sudo ../log/log_print change_ssid
 
 # run change_pwd.c
-sudo ./change_pwd $2
+sudo /home/pi/project/esp_smart_ap/admin/change_pwd $2
 
 # remove conf file and change bak file to conf file
 sudo rm /etc/hostapd/hostapd.conf
 sudo mv /etc/hostapd/hostapd.bak /etc/hostapd/hostapd.conf
 
 # Add a log text in log file
-sudo ../log/log_print change_pwd
+sudo /home/pi/project/esp_smart_ap/log/log_print change_ssid
+sudo /home/pi/project/esp_smart_ap/log/log_print change_pwd
+
+# backup file
+sudo /home/pi/project/esp_smart_ap/initial_bakup/initial_bakup.sh
